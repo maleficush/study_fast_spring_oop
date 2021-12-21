6b8d0925c18bc5ab9926f67b86bfca65c36d47d7
maleficush <maleficush@gmail.com>
2021-12-21 10:20:46 +0900

    Implement JavaSort
    자바 정렬 구현체를 단순히 이용한 정렬 클래스 작성

---
+++ src/main/java/com/fastcampus/javaoop/JavaSort.java
@@ -0,0 +1,14 @@
+package com.fastcampus.javaoop;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class JavaSort <T extends Comparable<T>>{
+	public List<T> sort(List<T> input){
+		List<T> output = new ArrayList<>(input);
+		Collections.sort(output);
+
+		return output;
+	}
+}
